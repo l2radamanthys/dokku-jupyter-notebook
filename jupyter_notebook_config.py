22dc6265405ca5da642c797d2a863dfb678c9269
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2023-04-14 12:27:58 -0300

    update config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -10,4 +10,4 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.allow_root = True
 c.NotebookApp.token = ''
 c.NotebookApp.password = os.getenv('NOTEBOOK_PASSWORD', '')
-c.NotebookApp.notebook_dir = os.getenv('NOTEBOOK_DIR', '/notebooks')
+c.NotebookApp.notebook_dir = os.getenv('NOTEBOOK_DIR', '.notebooks')
